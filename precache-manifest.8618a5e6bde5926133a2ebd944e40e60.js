b24a4ea3e6258f0bd858ef7453048de7f813f931
David van der Westhuizen <dcjwest@gmail.com>
2020-05-06 15:10:09 +0200

    Updates

---
+++ precache-manifest.8618a5e6bde5926133a2ebd944e40e60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803fa084a7bfd29412ac7552a8f1a5dd",
+    "url": "/tri-timer/index.html"
+  },
+  {
+    "revision": "8765f83728329e9f2a62",
+    "url": "/tri-timer/static/css/main.331c3555.chunk.css"
+  },
+  {
+    "revision": "c1217904e6ab95e046b4",
+    "url": "/tri-timer/static/js/2.aa5f54df.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/tri-timer/static/js/2.aa5f54df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8765f83728329e9f2a62",
+    "url": "/tri-timer/static/js/main.74149b5c.chunk.js"
+  },
+  {
+    "revision": "2016e5a1cd46f22d75e4",
+    "url": "/tri-timer/static/js/runtime-main.3fb0a6b2.js"
+  },
+  {
+    "revision": "a87a9f32f92ec29b1442c94c57de4dd4",
+    "url": "/tri-timer/static/media/pomodoro_alarm.a87a9f32.mp3"
+  }
+]);
