22d11f92b55e0e435a682904bfc66de8ef41af46
David van der Westhuizen <dcjwest@gmail.com>
2020-08-27 11:22:38 +0200

    Fixed z-index issues in loader and adjusted animation styles for smoother UX.

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -5,23 +5,26 @@ import './Loader.css';
 
 const Loader = () => {
     return (
-        <div className='loader container'>
-            <div className='loader-logo-wrapper container'>
-                <div className='loader-triangle-group container'>
-                    <div className='loader-triangle'></div>
-                    <div className='loader-triangle'></div>
-                    <div className='loader-triangle'></div>
+        <div className="loader container">
+            <div className="loader-bg"></div>
+            <div className="loader-logo-wrapper container">
+                <div className="loader-triangle-group container">
+                    <div className="loader-triangle"></div>
+                    <div className="loader-triangle"></div>
+                    <div className="loader-triangle"></div>
                 </div>
-                <div className='loader-circle container'></div>
-                <div className='loader-timer container'>
-                    <IconContext.Provider value={{className: 'loader-timer-icon'}}>
+                <div className="loader-circle container"></div>
+                <div className="loader-timer container">
+                    <IconContext.Provider
+                        value={{ className: 'loader-timer-icon' }}
+                    >
                         <MdAccessTime />
                     </IconContext.Provider>
                 </div>
             </div>
-            <div className='loader-text'>tri-timer</div>
+            <div className="loader-text">tri-timer</div>
         </div>
-    )
-}
+    );
+};
 
 export default Loader;
